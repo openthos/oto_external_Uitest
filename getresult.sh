d8a5152e2ff81fbd0e1f7586ee2b6a32b5b57859
zhanglinping <18701158269@163.com>
2017-09-27 09:40:36 +0800

    add adb connect

--- getresult.sh
+++ getresult.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-android_ip=192.168.0.64
+android_ip=192.168.0.90
 
+adb connect $android_ip
 date=`date +%Y%m%d`
 
 ./testgui.sh $android_ip 2>&1 | tee ./$date".log"
