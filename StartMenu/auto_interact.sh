63dcda196701de0c5bd98e76f319164193e104cf
zhanglinping <18701158269@163.com>
2017-10-19 09:59:30 +0800

    modify startmenu onenote testcase

--- StartMenu/auto_interact.sh
+++ StartMenu/auto_interact.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ##startmenuTest
 echo 'startmenutest'
-adb -s $ip_android:$adbPort push StartMenuTest.apk /data/local/tmp/com.example.qin.startmenutest || { echo "push StartMenuTest.apk failed" ; exit 1 ; }
-adb -s $ip_android:$adbPort shell pm install -r "/data/local/tmp/com.example.qin.startmenutest"
-adb -s $ip_android:$adbPort push StartMenuTest-androidTest.apk /data/local/tmp/com.example.qin.startmenutest.test || { echo "push StartMenuTest-androidTest.apk failed" ; exit 1 ; }
-adb -s $ip_android:$adbPort shell pm install -r "/data/local/tmp/com.example.qin.startmenutest.test"
-adb -s $ip_android:$adbPort shell am instrument -w -r   -e debug false -e class com.example.qin.startmenutest.StartMenuTest1 com.example.qin.startmenutest.test/android.support.test.runner.AndroidJUnitRunner
-adb -s $ip_android:$adbPort shell pm uninstall com.example.qin.startmenutest
+adb push StartMenuTest.apk /data/local/tmp/com.example.qin.startmenutest
+adb shell pm install -r "/data/local/tmp/com.example.qin.startmenutest"
+adb push StartMenuTest-androidTest.apk /data/local/tmp/com.example.qin.startmenutest.test
+adb shell pm install -r "/data/local/tmp/com.example.qin.startmenutest.test"
+adb shell am instrument -w -r -e debug false -e class com.example.qin.startmenutest.StartMenuTest1 com.example.qin.startmenutest.test/android.support.test.runner.AndroidJUnitRunner
+adb shell pm uninstall com.example.qin.startmenutest
 
--- onenote/src/com/autoTestUI/onenote.java
+++ onenote/src/com/autoTestUI/onenote.java
@@ -41,15 +41,15 @@ public class onenote extends UiAutomatorTestCase {
 		System.out.println("----------APP launch 时间： " + launchTime +"ms");
 		sleep(5000);
 
-		window_lib.windowtest(otoTest.mydevice,appName );
-
-		otoDisplayRun.execCmdNoSave("am start -n " + appName);
 		otoTest.ClickById("android:id/mwMaximizeBtn");
-		sleep(1000);
+		sleep(500);
 		otoTest.MoveToTop();
 		otoTest.ClickById("android:id/mwMaximizeBtn");
-		otoTest.ClickById("android:id/mwMinimizeBtn");
-		// 强制关闭程序
-		otoDisplayRun.execCmdNoSave("am force-stop " + appName.substring(0, appName.indexOf("/")));
+		otoTest.ClickById("");
+		
+		otoDisplayRun.execCmdNoSave("am start -n " + appName);
+		sleep(3000);
+		otoTest.ClickById("com.microsoft.office.onenote:id/negativeButton");
+		window_lib.windowtest(otoTest.mydevice,appName);
 	}
 }
